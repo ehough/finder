094e9bab8aaa9ae753e9cbffae1c8a4fb64e7962
Eric Hough <eric@tubepress.org>
2014-04-29 18:44:42 -0400

    Replacing overwritten test

--- src/main/php/ehough/finder/iterator/SortableIterator.php
+++ src/main/php/ehough/finder/iterator/SortableIterator.php
@@ -9,14 +9,12 @@
  * file that was distributed with this source code.
  */
 
-namespace Symfony\Component\Finder\Iterator;
-
 /**
- * SortableIterator applies a sort on a given Iterator.
+ * ehough_finder_iterator_SortableIterator applies a sort on a given Iterator.
  *
  * @author Fabien Potencier <fabien@symfony.com>
  */
-class SortableIterator implements \IteratorAggregate
+class ehough_finder_iterator_SortableIterator implements IteratorAggregate
 {
     const SORT_BY_NAME = 1;
     const SORT_BY_TYPE = 2;
@@ -30,45 +28,29 @@ class SortableIterator implements \IteratorAggregate
     /**
      * Constructor.
      *
-     * @param \Traversable     $iterator The Iterator to filter
-     * @param int|callback     $sort     The sort type (SORT_BY_NAME, SORT_BY_TYPE, or a PHP callback)
+     * @param Traversable     $iterator The Iterator to filter
+     * @param integer|callback $sort     The sort type (SORT_BY_NAME, SORT_BY_TYPE, or a PHP callback)
      *
-     * @throws \InvalidArgumentException
+     * @throws InvalidArgumentException
      */
-    public function __construct(\Traversable $iterator, $sort)
+    public function __construct(Traversable $iterator, $sort)
     {
         $this->iterator = $iterator;
 
         if (self::SORT_BY_NAME === $sort) {
-            $this->sort = function ($a, $b) {
-                return strcmp($a->getRealpath(), $b->getRealpath());
-            };
+            $this->sort = array($this, '_callbackSortName');
         } elseif (self::SORT_BY_TYPE === $sort) {
-            $this->sort = function ($a, $b) {
-                if ($a->isDir() && $b->isFile()) {
-                    return -1;
-                } elseif ($a->isFile() && $b->isDir()) {
-                    return 1;
-                }
-
-                return strcmp($a->getRealpath(), $b->getRealpath());
-            };
+            $this->sort = array($this, '_callbackSortType');
         } elseif (self::SORT_BY_ACCESSED_TIME === $sort) {
-            $this->sort = function ($a, $b) {
-                return ($a->getATime() > $b->getATime());
-            };
+            $this->sort = array($this, '_callbackSortTime');
         } elseif (self::SORT_BY_CHANGED_TIME === $sort) {
-            $this->sort = function ($a, $b) {
-                return ($a->getCTime() > $b->getCTime());
-            };
+            $this->sort = array($this, '_callbackSortChangedTime');
         } elseif (self::SORT_BY_MODIFIED_TIME === $sort) {
-            $this->sort = function ($a, $b) {
-                return ($a->getMTime() > $b->getMTime());
-            };
+            $this->sort = array($this, '_callbackSortModTime');
         } elseif (is_callable($sort)) {
             $this->sort = $sort;
         } else {
-            throw new \InvalidArgumentException('The SortableIterator takes a PHP callback or a valid built-in sort algorithm as an argument.');
+            throw new InvalidArgumentException('The ehough_finder_iterator_SortableIterator takes a PHP callback or a valid built-in sort algorithm as an argument.');
         }
     }
 
@@ -77,6 +59,37 @@ class SortableIterator implements \IteratorAggregate
         $array = iterator_to_array($this->iterator, true);
         uasort($array, $this->sort);
 
-        return new \ArrayIterator($array);
+        return new ArrayIterator($array);
+    }
+
+    public function _callbackSortModTime($a, $b)
+    {
+        return ($a->getMTime() > $b->getMTime());
+    }
+
+    public function _callbackSortChangedTime($a, $b)
+    {
+        return ($a->getCTime() > $b->getCTime());
+    }
+
+    public function _callbackSortTime($a, $b)
+    {
+        return ($a->getATime() > $b->getATime());
+    }
+
+    public function _callbackSortType($a, $b)
+    {
+        if ($a->isDir() && $b->isFile()) {
+            return -1;
+        } elseif ($a->isFile() && $b->isDir()) {
+            return 1;
+        }
+
+        return strcmp($a->getRealpath(), $b->getRealpath());
+    }
+
+    public function _callbackSortName($a, $b)
+    {
+        return strcmp($a->getRealpath(), $b->getRealpath());
     }
 }
